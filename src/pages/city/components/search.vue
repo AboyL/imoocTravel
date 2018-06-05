9a80a340897804931fdd87a63df30d125a4e97e8
L <1053801783@qq.com>
2018-06-05 18:33:53 +0800

    城市search输入框、加入一些变量

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+    <input type="text"
+           placeholder="请输入城市名称">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang='stylus'>
+@import '~styles/varibles.styl'
+.search
+  height 0.72rem
+  padding 0.1rem
+  background $bgColor
+  input
+    box-sizing border-box
+    border-radius .1rem
+    padding 0.1rem
+    $inputHeight = 0.62rem
+    width 100%
+    height $inputHeight
+    line-height $inputHeight
+    text-align center
+    color #666
+</style>
