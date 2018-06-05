a706d009749be33a6701d2fcf7db1401577d1809
L <1053801783@qq.com>
2018-06-05 19:25:35 +0800

    列表样式

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <ul class="item-list">
+        <li class="border-bottom">阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+      </ul>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <ul class="item-list">
+        <li class="border-bottom">阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+      </ul>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <ul class="item-list">
+        <li class="border-bottom">阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+      </ul>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <ul class="item-list">
+        <li class="border-bottom">阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+      </ul>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <ul class="item-list">
+        <li class="border-bottom">阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+        <li>阿拉善</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityList',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang='stylus'>
+.border-topbottom
+  &:before
+    border-color #cccccc
+  &:after
+    border-color #cccccc
+.border-bottom
+  &:before
+    border-color #cccccc
+.list
+  overflow hidden
+  position absolute
+  top 1.78rem
+  left 0
+  right 0
+  bottom 0
+  .title
+    line-height 0.44rem
+    background #eeeeee
+    padding-left 0.2rem
+    color #666666
+    font-size 0.26rem
+  .button-list
+    overflow hidden
+    padding 0.1rem 0.6rem 0.1rem 0.1rem
+    .button-wrapper
+      float left
+      width 33.3%
+      .button
+        margin 0.1rem
+        padding 0.1rem 0
+        text-align center
+        border 1px solid #ccc
+        border-radius 0.06rem
+  .item-list
+    li
+      line-height 0.77rem
+      padding-left 0.22rem
+</style>
