6225cc6ada63208b8a16194edc8fbbaf02f35f62
L <1053801783@qq.com>
2018-06-06 10:25:54 +0800

    字母表样式

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+ <div>
+   <ul class="list">
+     <li class="item">A</li>
+     <li class="item">B</li>
+     <li class="item">C</li>
+   </ul>
+</div>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang='stylus'>
+@import '~styles/varibles.styl'
+.list
+  display flex
+  flex-direction column
+  justify-content center
+  position absolute
+  top 1.76rem
+  right 0
+  bottom 0
+  width .4rem
+  .item
+    text-align center
+    color $bgColor
+    line-height .4rem
+</style>
